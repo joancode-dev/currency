699e7cad8417a02e211f3497b19d392b5e5bff1b
joan-ramirez <joan_montilla@hotmail.com>
2023-01-15 07:08:41 -0400

    Composer inti

---
+++ Currency.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Currency;
+
+use Goutte\Client;
+
+class Currency
+{
+    private $price;
+
+    private $client;
+
+    function __construct()
+    {
+        $this->client = new Client();
+    }
+
+    private function tractor(string $url)
+    {
+        $crawler = $this->client->request('GET', $url);
+
+        return $crawler->filter('#yDmH0d > c-wiz > div > div.e1AOyf > div > main > div.Gfxi4 > div.VfPpkd-WsjYwc.VfPpkd-WsjYwc-OWXEXe-INsAgc.KC1dQ.Usd1Ac.AaN0Dd.QZMA8b > c-wiz > div > div:nth-child(1) > div > div.rPF6Lc > div > div:nth-child(1) > div > span > div > div')->text();
+    }
+
+    public function change(string $from, string $to)
+    {
+        $url = "https://www.google.com/finance/quote/$from-$to";
+
+        $this->price = $this->tractor($url);
+
+        return $this;
+    }
+
+    public function amount(int $amount)
+    {
+        $this->price = $this->price * $amount;
+
+        return $this;
+    }
+
+    public function get()
+    {
+        return $this->price;
+    }
+}
